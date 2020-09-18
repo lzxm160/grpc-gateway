b886c6564f2d9a0762201c7ab9432325b4f30b53
lzxm160 <lzxm160@sina.com>
2020-09-18 16:25:31 +0800

    x

--- api/api.pb.gw.go
+++ api/api.pb.gw.go
@@ -9,7 +9,6 @@ It translates gRPC into RESTful JSON APIs.
 package api
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -249,17 +248,18 @@ func local_request_Wallet_CreateTransaction_1(ctx context.Context, marshaler run
 func request_Wallet_BroadcastTransaction_0(ctx context.Context, marshaler runtime.Marshaler, client WalletClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq core.Transaction
 	var metadata runtime.ServerMetadata
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(req.Body)
-	newStr := buf.String()
-
-	fmt.Printf(newStr)
+	//buf := new(bytes.Buffer)
+	//buf.ReadFrom(req.Body)
+	//newStr := buf.String()
+	//
+	//fmt.Printf(newStr)
 	newReader, berr := utilities.IOReaderFactory(req.Body)
 	if berr != nil {
 		fmt.Println("IOReaderFactory", berr)
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
 	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		fmt.Println("marshaler.NewDecoder(newReader()).Decode(&protoReq)", err)
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
