9cb4b967100a6bf762d56f9c0e9b682287d01301
lzxm160 <lzxm160@sina.com>
2020-09-20 15:14:00 +0800

    x

--- tron_http/main.go
+++ tron_http/main.go
@@ -16,8 +16,8 @@ import (
 
 var (
 	port = flag.Int("port", 50051, "port of your tron grpc service")
-	host = flag.String("host", "grpc.trongrid.io", "host of your tron grpc service")
-
+	//host = flag.String("host", "grpc.trongrid.io", "host of your tron grpc service")
+	host   = flag.String("host", "114.215.173.70", "host of your tron grpc service")
 	listen = flag.Int("listen", 38080, "the port that http server listen")
 )
 
